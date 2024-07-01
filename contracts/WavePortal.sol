57d764f9ae34e93de0f80e9ec4fd75e1918a2161
emgomes3112 <emgomes@outlook.com.br>
2024-07-01 14:05:43 -0300

    Comite do meu primeiro SmartContract

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+    constructor() {
+        console.log("Ueba, eu sou um contrato e eu sou inteligente");
+    }
+}
